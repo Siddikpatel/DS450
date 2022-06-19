471738599c06034fbfe48ef38228610325b5b1a1
Siddik Patel <patel.siddik369@gmail.com>
2022-06-19 17:57:23 +0530

    Merge sort program added

---
+++ Merge Sort/src/com/project/siddik/Main.java
@@ -0,0 +1,12 @@
+package com.project.siddik;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        int[] a1 = {3,1,0,-2,5};
+        int[] a2 = {11,90,12,6,-7,-8};
+
+
+    }
+}
